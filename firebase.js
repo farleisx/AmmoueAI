8568abd6e5878021c5ec9284ee94da617c39919f
farleisx <laroxmafox@gmail.com>
2026-01-09 13:16:29 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,28 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAmnZ69YDcEFcmuXIhmGxDUSPULxpI-Bmg",
-  authDomain: "ammoueai.firebaseapp.com",
-  projectId: "ammoueai",
-  storageBucket: "ammoueai.firebasestorage.app",
-  messagingSenderId: "135818868149",
-  appId: "1:135818868149:web:db9280baf9540a3339d5fc",
+// --- Firebase Config ---
+const firebaseConfig = {
+    apiKey: "AIzaSyAmnZ69YDcEFcmuXIhmGxDUSPULxpI-Bmg",
+    authDomain: "ammoueai.firebaseapp.com",
+    projectId: "ammoueai",
+    storageBucket: "ammoueai.firebasestorage.app",
+    messagingSenderId: "135818868149",
+    appId: "1:135818868149:web:db9280baf9540a3339d5fc",
 };
 
+// --- Initialize Firebase ---
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
+// --- Providers ---
 export const googleProvider = new GoogleAuthProvider();
 export const githubProvider = new GithubAuthProvider();
+
+// --- Auth State ---
+export function onAuthChange(callback) {
+    onAuthStateChanged(auth, callback);
+}
