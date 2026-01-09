1fc82b32643756f914a1a99b5ba84cd14279ac30
farleisx <laroxmafox@gmail.com>
2026-01-09 13:00:26 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAmnZ69YDcEFcmuXIhmGxDUSPULxpI-Bmg",
   authDomain: "ammoueai.firebaseapp.com",
   projectId: "ammoueai",
