0923a6e1f90e5ec0f0a164e86d55967f5e9d61ee
farleisx <laroxmafox@gmail.com>
2026-01-23 16:21:15 +0200

    Update fire_prompt.js

--- fire_prompt.js
+++ fire_prompt.js
@@ -1,3 +1,4 @@
+// fire_prompt.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import {
