5cd323fa613e38c011b0aceeb15c8606878fa9ba
farleisx <laroxmafox@gmail.com>
2026-02-12 18:09:49 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// firebase.js
+// firebase.js file
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
