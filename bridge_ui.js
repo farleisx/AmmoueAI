b4f7c776fa39458485ad828a6bef6e90eda0070c
farleisx <laroxmafox@gmail.com>
2026-02-12 11:59:23 +0200

    Update bridge_ui.js

--- bridge_ui.js
+++ bridge_ui.js
@@ -1,4 +1,4 @@
-// bridge_ui.js
+// bridge_ui.js file
 import { db } from "./fire_prompt.js";
 import { collection, query, orderBy, limit, onSnapshot } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
